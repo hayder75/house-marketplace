7e811cd02748017172f853e5586677e005c902a6
hayder75 <hayderaman75@gmail.com>
2023-02-05 21:30:48 -0500

    committed

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
 import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
+import Slider from '../componets/Slider'
 
 function Explore() {
   return (
@@ -10,6 +11,7 @@ function Explore() {
       </header>
 
       <main>
+   
         <p className='exploreCategoryHeading'>Categories</p>
         <div className='exploreCategories'>
           <Link to='/category/rent'>
